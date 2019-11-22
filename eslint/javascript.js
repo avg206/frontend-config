e439254681f41d62cd692e7d6d176a058c631b9e
Anatoli Huseu <avg.tolik@gmail.com>
2019-11-22 10:56:20 +0300

    feat: disable props types error for typescript

--- eslint/javascript.js
+++ eslint/javascript.js
@@ -1,16 +1,11 @@
 // eslint config for javascript
-const merge = require("lodash/merge");
+const merge = require('lodash/merge');
 
-const common = require("./index");
+const common = require('./index');
 
-module.exports = merge({
-  parser: "babel-eslint",
-  rules: {
-    "react/prop-types": [
-      "warn",
-      {
-        ignore: ["dispatch"]
-      }
-    ]
-  }
-}, common);
+module.exports = merge(
+  {
+    parser: 'babel-eslint',
+  },
+  common
+);
